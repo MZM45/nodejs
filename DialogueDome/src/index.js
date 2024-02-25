4196868ce901727c5121ebd3b3d972fae83ae1ed
Zain Momin <55015838+MZM45@users.noreply.github.com>
2024-02-26 03:01:33 +0500

    index.js

--- DialogueDome/src/index.js
+++ DialogueDome/src/index.js
@@ -5,7 +5,48 @@ const bodyParser = require('body-parser');
 const { LoginCollection, CommentCollection } = require('./config');
 const path = require("path");
 
-// Connect to the database
+mongoose.connect("mongodb+srv://MZM45:shesha007@dialoguedome.hdmwsjh.mongodb.net/?retryWrites=true&w=majority&appName=DialogueDome", { 
+   
+})
+.then(() => {
+    console.log("Database Connected Successfully");
+})
+.catch((err) => {
+    console.error("Error connecting to database:", err);
+});
+
+// Create Schema for Login
+const loginSchema = new mongoose.Schema({
+    username: {
+        type: String,
+      
+    },
+    password: {
+        type: String,
+      
+    }
+});
+
+// Create Schema for Comments
+const commentSchema = new mongoose.Schema({
+    commentText: {
+        type: String,
+       
+    },
+    username: {
+        type: String
+       
+    }
+});
+
+// Create models based on schemas
+const LoginCollection = mongoose.model("users", loginSchema);
+const CommentCollection = mongoose.model("usercomments", commentSchema);
+
+module.exports = {
+    LoginCollection,
+    CommentCollection
+};
 
 // for CSS
 
